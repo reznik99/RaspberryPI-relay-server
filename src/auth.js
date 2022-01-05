8ba48d9ac2a285512a333646e264e4dd06e7f2dc
Francesco <goras.francesco@gmail.com>
2022-01-05 21:27:51 +1300

    Websocket server code

--- src/auth.js
+++ src/auth.js
@@ -1,36 +1,39 @@
 
-const LocalStrategy = require('passport-local').Strategy;
-const JWTstrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local').Strategy
+const JWTstrategy = require('passport-jwt').Strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt
+const uuid = require('uuid')
+const { SECRET, LOGIN, PASSWORD } = require('./config/config')
 
-const { SECRET, USERNAME, PASSWORD } = require('./config/config');
-
-module.exports = (passport) => {
-    passport.serializeUser(function (user, done) {
-        done(null, user.id);
-    });
-
-    passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
-            done(err, user);
+module.exports = {
+    authInit: (passport) => {
+        passport.serializeUser(function (user, done) {
+            done(null, user.id)
         });
-    });
 
-    passport.use('login', new LocalStrategy({ usernameField: 'username', passwordField: 'password', session: false, }, (username, password, done) => {
-        // Invalid
-        if (username != USERNAME || password != PASSWORD)
-            return done(null, false, { message: 'Invalid username and/or password' });
+        passport.deserializeUser(function (id, done) {
+            User.findById(id, function (err, user) {
+                done(err, user)
+            });
+        });
 
-        // Valid
-        return done(null, user);
-    }))
+        passport.use('login', new LocalStrategy({ usernameField: 'username', passwordField: 'password', session: false, }, (username, password, done) => {
+            // Invalid
+            if (username != LOGIN || password != PASSWORD)
+                return done(null, false, { message: 'Invalid username and/or password' });
 
-    const opts = {
-        jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
-        secretOrKey: SECRET,
-    };
+            // Valid
+            const id = uuid.v4()
+            return done(null, { username, password, id });
+        }))
 
-    passport.use('jwt', new JWTstrategy(opts, (jwt_payload, done) => {
-        return done(null, jwt_payload);
-    }))
+        const opts = {
+            jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
+            secretOrKey: SECRET,
+        };
+
+        passport.use('jwt', new JWTstrategy(opts, (jwt_payload, done) => {
+            return done(null, jwt_payload);
+        }))
+    }
 }
---
+++ src/websocket.js
@@ -0,0 +1,78 @@
+const url = require('url')
+const wslib = require('ws')
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('./config/config')
+
+const wsClients = new Map()
+
+module.exports = {
+    wsClients: wsClients,
+    configureWebsocket: (expressServer) => {
+        // Define the WebSocket server. Here, the server mounts to the `/ws` route of the Express JS server.
+        const wss = new wslib.Server({ server: expressServer, path: '/ws' })
+
+
+        wss.on('connection', (ws, req) => {
+            const token = url.parse(req.url, true).query.token
+            try {
+                // Authenticate socket
+                const decoded = jwt.verify(token, SECRET)
+                console.log(decoded)
+                // Store socket
+                wsClients.set(decoded.username, ws)
+                ws.isAlive = true
+                ws.session = decoded
+                console.log(`New Websocket connection established for ${decoded.username}-${decoded.id}`)
+            } catch (err) {
+                console.error("Websocket connection rejected, invalid JWT")
+                ws.close()
+                return
+            }
+
+            // Handlers
+            ws.on('pong', () => { ws.isAlive = true })
+            ws.on('close', () => {
+                console.log(`Closing websocket for ${ws.session?.username}-${ws.session?.id}`)
+                wsClients.delete(ws.session?.id)
+                ws.close()
+            })
+            ws.on('message', (data) => {
+                try {
+                    console.log(data.toString())
+                    const decoded = jwt.verify(token, SECRET)
+                    // Do something with the messages
+                    try {
+                        const parsedData = JSON.parse(data)
+                        switch (parsedData.cmd.toUpperCase()) {
+                            case "MSG":
+                                const targetWS = wsClients.get(parsedData.target)
+                                if (!targetWS)
+                                    ws.send("User not online")
+                                else {
+                                    targetWS.send(ws.session.phone_no + ": " + parsedData.data)
+                                }
+                                break
+                            default:
+                                throw new Error("Invalid command")
+                        }
+                    } catch (err) {
+                        console.error(`Websocket ${ws.session?.username}-${ws.session?.id} invalid command`)
+                        ws.send("Invalid command")
+                    }
+                } catch (err) {
+                    console.error(`Websocket ${ws.session?.username}-${ws.session?.id} terminating, invalid JWT`)
+                    ws.close()
+                }
+            })
+        })
+
+        const interval = setInterval(() => {
+            wss.clients.forEach((ws) => {
+                console.log(`Pinging socket: ${ws.session?.username}-${ws.session?.id}`)
+                if (!ws.isAlive) return ws.terminate()
+                ws.isAlive = false
+                ws.ping(() => { })
+            })
+        }, 10000)
+    }
+}
