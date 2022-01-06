7094049287bb69a8193781f379f463107e9fa846
Francesco <goras.francesco@gmail.com>
2022-01-06 23:31:31 +1300

    More conversion for routes and auth code

---
+++ src/auth.ts
@@ -0,0 +1,30 @@
+
+const LocalStrategy = require('passport-local').Strategy
+const JWTstrategy = require('passport-jwt').Strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt
+import uuid from 'uuid'
+
+export const authInit = (passport) => {
+    passport.serializeUser(function (user, done) {
+        done(null, user.id)
+    })
+
+    passport.use('login', new LocalStrategy({ usernameField: 'username', passwordField: 'password', session: false, }, (username, password, done) => {
+        // Invalid
+        if (username != process.env.LOGIN || password != process.env.PASSWORD)
+            return done(null, false, { message: 'Invalid username and/or password' });
+
+        // Valid
+        const id = uuid.v4()
+        return done(null, { username, id });
+    }))
+
+    const opts = {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
+        secretOrKey: process.env.JWT_SECRET,
+    };
+
+    passport.use('jwt', new JWTstrategy(opts, (jwt_payload, done) => {
+        return done(null, jwt_payload);
+    }))
+}
